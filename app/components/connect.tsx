a18d0a39b714c88d7f59efac2414f187b1a2a1b9
Chris <victorchris73@gmail.com>
2025-05-03 20:06:37 +0100

    chore: ui fixes

--- app/components/connect.tsx
+++ app/components/connect.tsx
@@ -17,72 +17,75 @@ export default function Connect() {
   return (
     <section
       className="px-5 md:px-20 h-fit pb-20 font-inter pt-10"
-      data-aos="fade-right"
+      
     >
+      <div data-aos='fade-right'>
       <h1 className="text-xl md:text-4xl font-orbitron font-semibold">
         Keep in touch
       </h1>
       <div className="h-[2px] w-15 md:w-30 rounded-md mt-2 md:mt-4 bg-white"></div>
+      
       <p className="text-[16px] md:text-[30px] font-medium mt-10 text-center">
         Stay connected with me through my socials
       </p>
+      </div>
       <div
         className="flex flex-col justify-center items-center mt-10 gap-10"
-        data-aos="fade-up"
+        data-aos='fade-up'
       >
-        <Link href='https://x.com/cryptonautt_' target="_blank"><SocialCard>
-          <Image
-            src="/x-logo.png"
-            alt="x-logo"
-            width={100}
-            height={100}
-            className="w-[40px] bg-white p-1 rounded-md"
-          />
-          <p>
-            X/Twitter{" "}
-            <p  className="text-slate-400 hover:text-purple-400">
-              @cryptonautt_
+        <Link href="https://x.com/cryptonautt_" target="_blank">
+          <SocialCard>
+            <Image
+              src="/x-logo.png"
+              alt="x-logo"
+              width={100}
+              height={100}
+              className="w-[40px] bg-white p-1 rounded-md"
+            />
+            <p>
+              X/Twitter{" "}
+              <p className="text-slate-400 hover:text-purple-400">
+                @cryptonautt_
+              </p>
             </p>
-          </p>
-        </SocialCard>
+          </SocialCard>
         </Link>
 
-        <Link href='https://t.me/OxCrypt0n' target="_blank">
-        <SocialCard>
-          <Image
-            src="/telegram.png"
-            alt="telegram-logo"
-            width={100}
-            height={100}
-            className="w-[40px] bg-white p-1 rounded-md"
-          />
-          <p>
-            Telegram{" "}
-            <p  className="text-slate-400 hover:text-purple-400">
-              @OxCrpt0n
+        <Link href="https://t.me/OxCrypt0n" target="_blank">
+          <SocialCard>
+            <Image
+              src="/telegram.png"
+              alt="telegram-logo"
+              width={100}
+              height={100}
+              className="w-[40px] bg-white p-1 rounded-md"
+            />
+            <p>
+              Telegram{" "}
+              <p className="text-slate-400 hover:text-purple-400">@OxCrpt0n</p>
             </p>
-          </p>
-        </SocialCard>
+          </SocialCard>
         </Link>
 
-        <Link href='https://www.tiktok.com/@cryptonautt?_t=zm-8vbitnpa5q9&_r=1' target="_blank">
-        <SocialCard>
-          <Image
-            src="/tiktok.svg"
-            alt="tiktok-logo"
-            width={100}
-            height={100}
-            className="w-[40px] bg-white p-1 rounded-md"
-          />
-          <p>
-            TIktok{" "}
-            <p
-              className="text-slate-400 hover:text-purple-400"
-            >
-              @cryptonautt
+        <Link
+          href="https://www.tiktok.com/@cryptonautt?_t=zm-8vbitnpa5q9&_r=1"
+          target="_blank"
+        >
+          <SocialCard>
+            <Image
+              src="/tiktok.svg"
+              alt="tiktok-logo"
+              width={100}
+              height={100}
+              className="w-[40px] bg-white p-1 rounded-md"
+            />
+            <p>
+              TIktok{" "}
+              <p className="text-slate-400 hover:text-purple-400">
+                @cryptonautt
+              </p>
             </p>
-          </p>
-        </SocialCard>
+          </SocialCard>
         </Link>
       </div>
     </section>
